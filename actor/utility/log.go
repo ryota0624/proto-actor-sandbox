37f6107344cdb312896f522a5bc343e0587ddf33
ryota0624 <tennis.suta6@gmail.com>
2022-06-02 16:07:12 +0900

    ログ出力できるActorの実装

---
+++ actor/utility/log.go
@@ -0,0 +1,41 @@
+package utility
+
+import (
+	"fmt"
+	"github.com/asynkron/protoactor-go/actor"
+)
+
+type LogLevel string
+
+const (
+	INFO LogLevel = "INFO"
+)
+
+type Log struct {
+	Level  LogLevel
+	Format string
+	Data   []any
+}
+
+func NewLog(level LogLevel, format string, data []any) *Log {
+	return &Log{Level: level, Format: format, Data: data}
+}
+
+func InfoLog(str string) Log {
+	return Log{Level: INFO, Format: str, Data: nil}
+}
+
+func InfoLogf(format string, data []any) Log {
+	return Log{Level: INFO, Format: format, Data: data}
+}
+
+type LoggingActor struct{}
+
+func (state *LoggingActor) Receive(context actor.Context) {
+	switch msg := context.Message().(type) {
+	case Log:
+		fmt.Printf("[%s] ", msg.Level)
+		fmt.Printf(msg.Format, msg.Data...)
+		fmt.Println()
+	}
+}
